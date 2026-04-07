5cd2fd25a1cd6ffc5751191730d25ec36af6bc2f
tyler tran <tyler.tran88@yahoo.com>
2026-04-07 10:43:38 -0700

    renamed knowledge_base_file

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from gui import GUI
 from decision_tree import DecisionTree
 import tkinter as tk
 
-knowledge_base_file = "knowlege_base.json"
+knowledge_base_file = "knowledge_base.json"
 
 def main():
     tree = DecisionTree(knowledge_base_file)
